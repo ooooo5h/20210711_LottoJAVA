64605c3e60447fb57002f280cd9c1904d95a68c5
전은형 <ooooo5h91@gmail.com>
2021-07-11 20:25:49 +0900

    3. 맞춘 갯수에 따른 등수 표현까지

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -40,5 +40,67 @@ public class MainDrive {
 				}
 			}		
 		}
+		
+		
+		int[] lottoNumbers = new int[6];
+		
+		for(int i = 0 ; i < lottoNumbers.length; i++) {
+			
+			while(true) {
+				
+				int lottoNumber = (int)(Math.random()*45+1);
+				
+				boolean isDuplicateOk = true;
+				
+				for(int num : lottoNumbers) {
+					if(num == lottoNumber) {
+						isDuplicateOk = false;
+						break;
+					}
+				}
+				if(isDuplicateOk) {
+					lottoNumbers[i] = lottoNumber;
+					break;
+				}	
+			}
+		}
+		
+		int sameNumberCount = 0;
+		
+//		내입력:1,2,3,4,5,6    당첨:4,5,6,7,8,9  
+//		1,4 1,5 1,6 ... = 바쁜 반복 j
+//		1-2-3--4-...    = 덜 바쁜 반복 i
+//		for(int i = 0 ; i < myLottoNumbers.length; i++ ) {
+//			for(int j= 0 ; j < lottoNumbers.length; j++) {
+//				
+//				if(myLottoNumbers[i] == lottoNumbers[j]) {
+//					sameNumber++;
+//				}
+//			}
+//		}   -> 로또게임에서 위치는 상관없어서 굳이 i,j 반복문 안써도 됨
+		
+		for(int num : myLottoNumbers) {
+			for(int lottoNum : lottoNumbers) {
+				if(num == lottoNum) {
+					sameNumberCount++;
+				}
+			}
+		}
+		
+		if(sameNumberCount==6) {
+			System.out.println("1등");
+		}
+		else if(sameNumberCount==5) {
+			System.out.println("3등");
+		}
+		else if(sameNumberCount==4) {
+			System.out.println("4등");
+		}
+		else if(sameNumberCount==3) {
+			System.out.println("5등");
+		}
+		else {
+			System.out.println("낙첨");
+		}		
 	}
 }
